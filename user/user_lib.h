63ee6f23eafdb352022b52b3345d6b2947ecf65c
walk_alone <2390335608@qq.com>
2023-01-07 10:04:57 +0800

    Finish of Experiment

---
+++ user/user_lib.h
@@ -0,0 +1,12 @@
+/*
+ * header file to be used by applications.
+ */
+
+int printu(const char *s, ...);
+int exit(int code);
+void* naive_malloc();
+void naive_free(void* va);
+int fork();
+void yield();
+void print_backtrace(int depth);
+ssize_t wait(int pid);
