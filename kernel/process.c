cd52ab74ec998c1760d94cc18b2bf5d229812f11
walk_alone <2390335608@qq.com>
2023-02-19 15:11:16 +0800

    My work on lab1 challeng2 has done.

---
+++ kernel/process.c
@@ -0,0 +1,93 @@
+/*
+ * Utility functions for process management. 
+ *
+ * Note: in Lab1, only one process (i.e., our user application) exists. Therefore, 
+ * PKE OS at this stage will set "current" to the loaded user application, and also
+ * switch to the old "current" process after trap handling.
+ */
+
+#include "riscv.h"
+#include "strap.h"
+#include "config.h"
+#include "process.h"
+#include "elf.h"
+#include "string.h"
+
+#include "spike_interface/spike_utils.h"
+
+//Two functions defined in kernel/usertrap.S
+extern char smode_trap_vector[];
+extern void return_to_user(trapframe*);
+
+// current points to the currently running user-mode application.
+process* current = NULL;
+
+//
+// switch to a user-mode process
+//
+void switch_to(process* proc) {
+  assert(proc);
+  current = proc;
+
+  write_csr(stvec, (uint64)smode_trap_vector);
+  // set up trapframe values that smode_trap_vector will need when
+  // the process next re-enters the kernel.
+  proc->trapframe->kernel_sp = proc->kstack;  // process's kernel stack
+  proc->trapframe->kernel_trap = (uint64)smode_trap_handler;
+
+  // set up the registers that strap_vector.S's sret will use
+  // to get to user space.
+
+  // set S Previous Privilege mode to User.
+  unsigned long x = read_csr(sstatus);
+  x &= ~SSTATUS_SPP;  // clear SPP to 0 for user mode
+  x |= SSTATUS_SPIE;  // enable interrupts in user mode
+
+  write_csr(sstatus, x);
+
+  // set S Exception Program Counter to the saved user pc.
+  write_csr(sepc, proc->trapframe->epc);
+
+  // switch to user mode with sret.
+  return_to_user(proc->trapframe);
+}
+
+
+
+// print the given line
+void print_line(addr_line *line) {
+  char filepath[1000], code[2000];
+  struct stat temp;
+  int l = strlen(current->dir[current->file[line->file].dir]);
+  strcpy(filepath, current->dir[current->file[line->file].dir]); // copy the file path
+  filepath[l] = '/';
+  strcpy(filepath + l + 1, current->file[line->file].file); // concat the file name
+  
+  // read the code line and print
+  spike_file_t *f = spike_file_open(filepath, O_RDONLY, 0);
+  spike_file_stat(f, &temp);
+  spike_file_read(f, code, temp.st_size);
+  spike_file_close(f);
+  int cnt = 0;
+  for (int i = 0; i < temp.st_size;) {
+    int j = i;
+    while (j < temp.st_size && code[j] != '\n') j++;
+    cnt++;
+    if (cnt == line->line - 1) {
+      code[j] = '\0';
+      sprint("Runtime error at %s:%d\n%s\n", filepath, line->line, code + i);
+      break;
+    }
+    i = j + 1;
+  }
+}
+
+void print_error_line() {
+  uint64 mepc = read_csr(mepc);
+  for (int i = 0; i < current->line_ind; i++) {
+    if (mepc < current->line[i].addr) {
+      print_line(current->line + i - 1);
+      break;
+    }
+  }
+}
---
+++ kernel/process.h
@@ -0,0 +1,43 @@
+#ifndef _PROC_H_
+#define _PROC_H_
+
+#include "riscv.h"
+
+typedef struct trapframe {
+  // space to store context (all common registers)
+  /* offset:0   */ riscv_regs regs;
+
+  // process's "user kernel" stack
+  /* offset:248 */ uint64 kernel_sp;
+  // pointer to smode_trap_handler
+  /* offset:256 */ uint64 kernel_trap;
+  // saved user process counter
+  /* offset:264 */ uint64 epc;
+}trapframe;
+
+// code file struct, including directory index and file name char pointer
+typedef struct {
+    uint64 dir; char *file;
+} code_file;
+// address-line number-file name table
+typedef struct {
+    uint64 addr, line, file;
+} addr_line;
+
+// the extremely simple definition of process, used for begining labs of PKE
+typedef struct process {
+  // pointing to the stack used in trap handling.
+  uint64 kstack;
+  // trapframe storing the context of a (User mode) process.
+  trapframe* trapframe;
+  char *debugline; char **dir; code_file *file; addr_line *line; int line_ind;
+}process;
+
+void switch_to(process*);
+
+extern process* current;
+
+void print_line(addr_line *line);
+void print_error_line();
+
+#endif
